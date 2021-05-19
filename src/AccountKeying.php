e55df7d549a34d018053bfeadd6de894ee04e3ff
Simbiat <simbiat@outlook.com>
2021-05-19 12:46:45 +0300

    Minor mistypes correction

--- src/AccountKeying.php
+++ src/AccountKeying.php
@@ -1,13 +1,13 @@
 <?php
 declare(strict_types=1);
 namespace Simbiat;
-#test
+
 class AccountKeying
 {
-    public function accCheck(string $newnum, string $account, ?int $bic_check = null): int|bool
+    public function accCheck(string $bic_num, string $account, ?int $bic_check = null): int|bool
     {
         #Validate values
-        if (preg_match('/^[0-9]{9}$/', $newnum) !== 1) {
+        if (preg_match('/^[0-9]{9}$/', $bic_num) !== 1) {
             return false;
         }
         if (preg_match('/^[0-9]{5}[0-9АВСЕНКМРТХавсенкмртх][0-9]{14}$/', $account) !== 1) {
@@ -15,14 +15,14 @@ class AccountKeying
         }
         $VK = [7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1];
         $rkcNum = [];
-        $mult = [];
-        $summ = 0;
+        $multi = [];
+        $sum = 0;
         #Strings to arrays
-        $newnum_split = str_split($newnum);
+        $bic_num_split = str_split($bic_num);
         $account_split = str_split(strtoupper($account));
         #Get current key
         $currKey = $account_split[8];
-        #Some special accounts can have letters in them (although I have not seen any myself). They need to be replaced with regulat numbers as per specification
+        #Some special accounts can have letters in them (although I have not seen any myself). They need to be replaced with regular numbers as per specification
         $account_split[5] = match($account_split[5]) {
             'A', 'а' => 0,
             'B', 'в' => 1,
@@ -37,14 +37,14 @@ class AccountKeying
             default => $account_split[5],
         };
         #RKC
-        if(intval($newnum_split[6].$newnum_split[7].$newnum_split[8]) <= 2) {
+        if(intval($bic_num_split[6].$bic_num_split[7].$bic_num_split[8]) <= 2) {
             $rkcNum[0] = 0;
-            $rkcNum[1] = intval($newnum_split[4]);
-            $rkcNum[2] = intval($newnum_split[5]);
+            $rkcNum[1] = intval($bic_num_split[4]);
+            $rkcNum[2] = intval($bic_num_split[5]);
         } else {
-            $rkcNum[0] = intval($newnum_split[6]);
-            $rkcNum[1] = intval($newnum_split[7]);
-            $rkcNum[2] = intval($newnum_split[8]);
+            $rkcNum[0] = intval($bic_num_split[6]);
+            $rkcNum[1] = intval($bic_num_split[7]);
+            $rkcNum[2] = intval($bic_num_split[8]);
         }
         if (is_null($bic_check)) {
             $account_split[8] = 0;
@@ -53,18 +53,18 @@ class AccountKeying
         $fullStr = array_merge($rkcNum, $account_split);
         #Multiplication
         for ($i = 0; $i < 23; $i++) {
-            $mult[$i]=intval($fullStr[$i])*$VK[$i];
+            $multi[$i]=intval($fullStr[$i])*$VK[$i];
         }
         #Summing
         for ($i = 0; $i < 23; $i++) {
-            $summ += intval(str_split(strval($mult[$i]))[(count(str_split(strval($mult[$i]))) - 1)]);
+            $sum += intval(str_split(strval($multi[$i]))[(count(str_split(strval($multi[$i]))) - 1)]);
         }
         #Second character
-        $secCh = intval(str_split(strval($summ))[(count(str_split(strval($summ))) - 1)]);
+        $secCh = intval(str_split(strval($sum))[(count(str_split(strval($sum))) - 1)]);
         if (is_null($bic_check)) {
             $secCh = $secCh*3;
             $secCh= intval(str_split(strval($secCh))[(count(str_split(strval($secCh))) - 1)]);
-            return $this->accCheck($newnum, $account, $secCh);
+            return $this->accCheck($bic_num, $account, $secCh);
         } else {
             if ($currKey == $bic_check && $secCh == 0) {
                 return true;
